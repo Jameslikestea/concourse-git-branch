d1d7be95198b661b979b5204a480c44224c86b83
James Crocker <5156202+Jameslikestea@users.noreply.github.com>
2023-02-25 20:48:06 +0000

    adding some randomization

--- check/main.go
+++ check/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("[{\"ref\":\"399a572\",\"branch\":\"main\"}]")
+	data := make([]byte, 10)
+	rand.Read(data)
+	s := fmt.Sprintf("%x", sha256.Sum256(data))
+
+	fmt.Printf("[{\"ref\":\"399a572\",\"branch\":\"main\"},{\"ref\":\"%s\",\"branch\":\"develop\"}]\n", s[:7])
 }
