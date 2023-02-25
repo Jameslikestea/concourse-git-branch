ad417975cbd5f1d74e000913249c409608b4cc9c
James Crocker <5156202+Jameslikestea@users.noreply.github.com>
2023-02-25 21:41:59 +0000

    checking git

--- check/main.go
+++ check/main.go
@@ -6,6 +6,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/go-git/go-git/v5/storage/memory"
+
 	"github.com/Jameslikestea/concourse-git-branch/models"
 )
 
@@ -15,6 +20,31 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	json.NewEncoder(os.Stderr).Encode(req)
+
+	repo := git.NewRemote(
+		memory.NewStorage(), &config.RemoteConfig{
+			Name: "origin",
+			URLs: []string{req.Source.URI},
+		},
+	)
+
+	pkey, err := ssh.NewPublicKeys("git", []byte(req.Source.PrivateKey), "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	refs, err := repo.List(
+		&git.ListOptions{
+			Auth: pkey,
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, ref := range refs {
+		fmt.Fprintf(os.Stderr, "%s: %s", ref.Name().Short(), ref.Hash().String())
+	}
+
 	fmt.Printf("[]\n")
 }
